573eacb3b1c531278dd45f1496a25e370df179c6
honghoon <wjddj1@naver.com>
2025-06-12 23:31:09 +0900

    tailwind 추가 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ export default <Config>{
     './pages/**/*.{vue,js,ts,jsx,tsx}',
     './components/**/*.{vue,js,ts,jsx,tsx}'
   ],
+  safelist: [
+    'bg-purple-100','text-purple-700','bg-purple-400',
+    'bg-teal-100','text-teal-700','bg-teal-400',
+    'bg-blue-100','text-blue-700','bg-blue-400',
+    'bg-green-100','text-green-700','bg-green-400'
+    // 더 많은 색상 추가 가능
+  ],
   theme: {
     extend: {
         fontFamily: {
