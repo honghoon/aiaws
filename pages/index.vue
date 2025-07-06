aefc93c67d6627ad2a4b20a6617fca2d86da7bcf
honghoon <wjddj1@naver.com>
2025-07-06 20:54:05 +0900

    기본 페이지 변경

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="p-8 text-center">
-    <h1 class="text-3xl font-bold text-blue-600">홈 페이지</h1>
-    <n-button class="mt-4" @click="navigate">/about로 이동</n-button>
-  </div>
+
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
 
-  // definePageMeta({
-  //     layout: "default", // 기본 레이아웃 적용
-  // });
-
-const router = useRouter()
-const navigate = () => {
-  router.push('/about')
-}
+onMounted(() => {
+  navigateTo('/KanbanBoard')
+})
 </script>
