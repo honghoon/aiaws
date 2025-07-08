1c414f891abf26d14fd5c0b3f9355656f8172da1
admin <admin>
2025-07-08 13:41:53 +0900

    vercel 관련 개선

--- server/utils/bedrock_stream.ts
+++ server/utils/bedrock_stream.ts
@@ -90,7 +90,7 @@ function sleep(ms: number) {
 }
 
 export async function streamFallbackMessageJump(writer: ServerResponse, message: string) {
-  const maxChunkLength = 300;
+  const maxChunkLength = 10;
 
   // 1. \n 기준으로 문단 단위로 쪼갬
   const paragraphs = message.split('\n');
